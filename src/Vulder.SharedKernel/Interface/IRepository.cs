7c2712311c0d9ac4f80cb53ea5c739cd0e66bb1c
MRmlik12 <mrmlik12sup@gmail.com>
2021-06-24 22:06:35 +0200

    Add more methods to implement in IRepository.cs

--- src/Vulder.SharedKernel/Interface/IRepository.cs
+++ src/Vulder.SharedKernel/Interface/IRepository.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace Vulder.SharedKernel.Interface
@@ -5,7 +6,8 @@ namespace Vulder.SharedKernel.Interface
     public interface IRepository
     {
         Task<T> AddAsync<T>(T entity) where T : BaseEntity, IAggregateRoot;
-        Task<T> GetByNameAsync<T>(string name) where T : BaseEntity, IAggregateRoot;
+        Task<T> GetByIdAsync<T>(T entity) where T : BaseEntity, IAggregateRoot;
+        Task<List<T>> ListAsync<T>() where T : BaseEntity, IAggregateRoot;
         Task UpdateAsync<T>(T entity) where T : BaseEntity, IAggregateRoot;
         Task DeleteAsync<T>(T entity) where T : BaseEntity, IAggregateRoot;
     }
